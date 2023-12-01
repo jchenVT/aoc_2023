6eac3b29e539d9f9fca71ac99085e9839142f00e
jchenvt <jchenvt@gmail.com>
2023-12-01 12:49:50 -0500

    day1 solution

---
+++ day1.py
@@ -0,0 +1,20 @@
+import re
+def part1(file):
+    sum = 0
+    for line in file:
+        nums = re.findall(r'\d', line)
+        sum += int(f"{nums[0]}{nums[-1]}")
+    print(sum)
+                         
+def part2(file):
+    numbers = {"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
+    sum = 0
+    for line in file:
+        nums = re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)
+        print (line[:-1])
+        print (nums)
+        sum += (numbers[nums[0]] * 10) + numbers[nums[-1]]
+    print(sum)
+            
+with open("input_day1") as f:
+    part2(f)
